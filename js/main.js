a1ed3710cd15d6e1e0d900c0f86539933b8aa566
Lenkeri <Lno4kamalt@yandex.ru>
2021-05-31 17:06:03 +0500

    homework 1 page 1 and 2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,4 @@
-import {random_integer} from './util.js'
-
-
-
-let objTemplateMok = {
-  "author": {
-      "avatar": '',
-  },
-  "offer": {
-      "title": '',
-      "address": '',
-      "price": '',
-      "type": '',
-      "rooms": '',
-      "guests":'',
-      "checkin": '',
-      "checkout":'',
-      "features":'',
-      "description": '',
-      "photos": ''
-  },
-  "location": {
-      "x": '',
-      "y": ''
-  }
-}
+import {random_integer, getRandomItem, getArrRandomLength} from './util.js'
 
 const TITLES = [
   'Большая уютная квартира',
@@ -36,51 +11,55 @@ const TITLES = [
   'Неуютное бунгало по колено в воде',
 ];
 
-const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"]
+function generateAd (){
+  return  {
+    "author": {
+        "avatar": '',
+    },
+    "offer": {
+        "title": '',
+        "address": '',
+        "price": '',
+        "type": '',
+        "rooms": '',
+        "guests":'',
+        "checkin": '',
+        "checkout":'',
+        "features":'',
+        "description": '',
+        "photos": ''
+    },
+    "location": {
+        "x": '',
+        "y": ''
+    }
+  }
+}
 
-// {
-//   "author": {
-//       "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-//   },
-//   "offer": {
-//       "title": строка, заголовок предложения
-//       "address": строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
-//       "price": число, стоимость
-//       "type": строка с одним из четырёх фиксированных значений: palace, flat, house или bungalow
-//       "rooms": число, количество комнат
-//       "guests": число, количество гостей, которое можно разместить
-//       "checkin": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
-//       "checkout": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-//       "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-//       "description": строка с описанием,
-//       "photos": массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-//   },
-//   "location": {
-//       "x": случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-//       "y": случайное число, координата y метки на карте от 130 до 630.
-//   }
-// }
-// const getArrAd = function(){
-//   console.log('jngjfgg');
+const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+const CHECKIN = ['12:00', '13:00', '14:00']
+const mapOverlay = document.querySelector('.map__overlay');
+const map = document.querySelector('.map')
+map.classList.remove('map--faded')
 
-//   }
+const PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
+
 let arrAd = [];
-console.log(getArrAd());
 
-let author = objTemplateMok.author;
-let offer = objTemplateMok.offer;
-let lacation = objTemplateMok.location;
-
-
-function getArrAd(template, num) {
-  console.log(template);
-
+function getArrAd(num) {
   for (let i = 0; i < num; i++){
-    console.log(template);
 
-    author.avatar = `img/avatars/user0${i}.png`
+    let template = generateAd()
+
+    let author = template.author;
+    let offer = template.offer;
+    let location = template.location;
+    template.author.avatar = `img/avatars/user0${i}.png`
+    author.avatar = `img/avatars/user0${i}.png`;
+
     offer.title = TITLES[i];
-    // offer.address(`${Window.x} ${Window.y}`)
+    offer.address = `${location.x} ${location.y}`
+    offer.price = random_integer(2000, 50000)
      if (i === 2 || i === 3){
        offer.type = 'palace'
      } else if(i === 0 || i === 1){
@@ -91,9 +70,20 @@ function getArrAd(template, num) {
      } else {
        offer.type = 'bungalow'
      }
+    offer.rooms = random_integer(1, 10);
+    offer.guests = offer.rooms;
+    offer.checkin = getRandomItem(CHECKIN);
+    offer.checkout = offer.checkin;
+    offer.features = getArrRandomLength(FEATURES);
+    offer.description = TITLES[i];
+    offer.photos = getArrRandomLength(PHOTOS);
+    location.x = random_integer(1, mapOverlay.clientWidth)
+    location.y = random_integer(130, 630)
     arrAd.push(template)
-  }
 
+
+  }
 }
-  getArrAd(objTemplateMok, 8)
+  getArrAd(8)
   console.log(arrAd)
+
--- js/util.js
+++ js/util.js
@@ -3,3 +3,18 @@ export function random_integer(min, max) {
   return Math.round(rand);
 }
 
+export const getRandomItem = function(arr) {
+  const randomIndex = random_integer(1, arr.length - 1);
+  return arr[randomIndex];
+}
+
+export const getArrRandomLength = function(arr){
+let newArr = []
+for (let i = 0; i < arr.length ; i++){
+  let num = random_integer(0, 1)
+  if(num == true){
+    newArr.push(arr[i])
+  }
+}
+return newArr
+}
