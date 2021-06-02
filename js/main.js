1b86d029eaccc5e4bf5c42757e9bf32df6b07596
Lenkeri <Lno4kamalt@yandex.ru>
2021-06-02 20:48:25 +0500

    немного 1го задания

--- js/main.js
+++ js/main.js
@@ -41,6 +41,31 @@ const CHECKIN = ['12:00', '13:00', '14:00']
 const mapOverlay = document.querySelector('.map__overlay');
 const map = document.querySelector('.map')
 map.classList.remove('map--faded')
+const templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
+console.log(templatePin);
+const createPinElement = function(obj, temp){
+  const pin = temp.cloneNode(true);
+  // let pin
+  //  pin =  pinTemplate.content.cloneNode(true);
+  // console.log(pin, 2);
+  pin.img = `${obj.author.avatar}`
+  pin.alt= `${obj.offer.title}`
+  console.log(pin, 565655654545)
+  // pin.style = `left: ${obj.location.x + pin.width / 2}px; top: ${location.y + pin.height}px;`
+// .style.left = `${obj.location.x + pin.width / 2}px`
+// .style.top = `${location.y + pin.height}`;
+
+}
+// const displayWizards = (arr) => {
+//   similarList.innerHTML = '';
+//   for (let i = 0; i < arr.length; i += 1) {
+//     similarList.appendChild(createWizardElement(arr[i]));
+//   }
+// }
+ function getNewPin(){
+ return document.querySelector('#pin').content.querySelector('.map__pin');
+}
+const mapPins = document.querySelector('.map__pins')
 
 const PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
 
@@ -78,9 +103,12 @@ function getArrAd(num) {
     offer.description = TITLES[i];
     offer.photos = getArrRandomLength(PHOTOS);
     location.x = random_integer(1, mapOverlay.clientWidth)
-    location.y = random_integer(130, 630)
+    location.y = random_integer(130, 630);
     arrAd.push(template)
-
+    // console.log()
+    const element = createPinElement(template, getNewPin());
+    // mapPins.appendChild( createPinElement(template, getNewPin()));
+    // console.log(element, 'el')
 
   }
 }
