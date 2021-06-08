b7ed85c9fc8d65e101074612e29ea0e0b60bb08a
Lenkeri <lno4kamalt@yandex.ru>
2021-06-08 18:44:28 +0500

    рендеринг для большего качества и затрат на производительность, читабельность

---
+++ js/data.js
@@ -0,0 +1,51 @@
+import {random_integer, getRandomItem, getArrRandomLength} from './util.js'
+
+const TITLES = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде',
+];
+const PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
+const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+const CHECKIN = ['12:00', '13:00', '14:00'];
+const TYPE_OFFER = ['palace', 'flat', 'house', 'bungalow'];
+
+function generateAd(i) {
+  const x = random_integer(1, 1200);
+  const y = random_integer(130, 630)
+  return  {
+    author: {
+      "avatar": `img/avatars/user0${i + 1}.png`,
+    },
+    offer: {
+      title: TITLES[i],
+      address: `${x} ${location.y}`,
+      price: random_integer(2000, 50000),
+      type: getRandomItem(TYPE_OFFER),
+      rooms: random_integer(1, 10),
+      guests:  random_integer(1, 10),
+      checkin: getRandomItem(CHECKIN),
+      checkout:getRandomItem(CHECKIN),
+      features: getArrRandomLength(FEATURES),
+      description: TITLES[i],
+      photos: getArrRandomLength(PHOTOS)
+    },
+    location: {
+      x,
+      y
+    }
+  }
+}
+
+export const generateAds = (num) => {
+  const arr = [];
+  for(let i = 0; i < num; i += 1) {
+    arr.push(generateAd(i));
+  }
+  return arr;
+}
--- js/main.js
+++ js/main.js
@@ -1,44 +1,5 @@
-import {random_integer, getRandomItem, getArrRandomLength} from './util.js'
+import {generateAds} from './data.js'
 
-const TITLES = [
-  'Большая уютная квартира',
-  'Маленькая неуютная квартира',
-  'Огромный прекрасный дворец',
-  'Маленький ужасный дворец',
-  'Красивый гостевой домик',
-  'Некрасивый негостеприимный домик',
-  'Уютное бунгало далеко от моря',
-  'Неуютное бунгало по колено в воде',
-];
-
-function generateAd (){
-  return  {
-    "author": {
-        "avatar": '',
-    },
-    "offer": {
-        "title": '',
-        "address": '',
-        "price": '',
-        "type": '',
-        "rooms": '',
-        "guests":'',
-        "checkin": '',
-        "checkout":'',
-        "features":'',
-        "description": '',
-        "photos": ''
-    },
-    "location": {
-        "x": '',
-        "y": ''
-    }
-  }
-}
-
-const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-const CHECKIN = ['12:00', '13:00', '14:00']
-const mapOverlay = document.querySelector('.map__overlay');
 const map = document.querySelector('.map')
 map.classList.remove('map--faded')
 const templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -49,57 +10,18 @@ const createPinElement = function(obj){
     pin.style.top = `${obj.location.y - templatePin.style.height}px`;
     pin.children[0].alt= `${obj.offer.title}`
 
-mapPins.appendChild(pin)
-
-}
-
- function getNewPin(){
- return document.querySelector('#pin').content.querySelector('.map__pin');
+    return pin
 }
 const mapPins = document.querySelector('.map__pins')
 
-const PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
-
-let arrAd = [];
-
-function getArrAd(num) {
-  for (let i = 0; i < num; i++){
-
-    let template = generateAd()
-
-    let author = template.author;
-    let offer = template.offer;
-    let location = template.location;
-    author.avatar = `img/avatars/user0${i + 1}.png`;
-
-    offer.title = TITLES[i];
-    offer.address = `${location.x} ${location.y}`
-    offer.price = random_integer(2000, 50000)
-     if (i === 2 || i === 3){
-       offer.type = 'palace'
-     } else if(i === 0 || i === 1){
-      offer.type = 'flat'
-
-     } else if (i === 4 || i === 5){
-       offer.type = 'house'
-  } else {
-       offer.type = 'bungalow'
-     }
-    offer.rooms = random_integer(1, 10);
-    offer.guests = offer.rooms;
-    offer.checkin = getRandomItem(CHECKIN);
-    offer.checkout = offer.checkin;
-    offer.features = getArrRandomLength(FEATURES);
-    offer.description = TITLES[i];
-    offer.photos = getArrRandomLength(PHOTOS);
-    location.x = random_integer(1, mapOverlay.clientWidth)
-    location.y = random_integer(130, 630);
-    arrAd.push(template)
-    createPinElement(template);
-
+const getPinsForMap = function(num){
+  const arr = generateAds(num)
+  for(let i = 0; i < num;i ++){
+  mapPins.appendChild(createPinElement(arr[i]))
 
   }
 }
-  getArrAd(8)
-  // console.log(arrAd)
+
+getPinsForMap(8)
+
 
