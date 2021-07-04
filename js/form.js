4b9760aad5c44b7794e7d682895fc5ecc9d044a4
Lenkeri <Lno4kamalt@yandex.ru>
2021-07-04 21:21:11 +0500

    валидация цены

--- js/form.js
+++ js/form.js
@@ -3,6 +3,10 @@ const addrInput = form.querySelector('#address');
 const pinMap = document.querySelector('.map__pin--main');
 const numberRoom = form.querySelector('.room-number');
 const formRoom = form.querySelector('#room_number');
+const homeType = form.querySelector('#type');
+const priceType = form.querySelector('#price');
+const guest = form.querySelector('#capacity');
+
 export function getDisableForm () {
   const arr = form.querySelectorAll('fieldset')
   form.classList.add('ad-form--disabled')
@@ -11,33 +15,32 @@ export function getDisableForm () {
   }
 }
 
-
-
 export const getUndisableForm = function () {
   const arr = form.querySelectorAll('fieldset')
   form.classList.remove('ad-form--disabled')
   for(let i = 0; i < arr.length; i++){
     arr[i].removeAttribute('disabled')
   }
-  // addrInput.value = pinMap.pageX;
+
   let coordinatePin = pinMap.getBoundingClientRect()
   let pinX = coordinatePin.x + pinMap.style.width / 2;
   let pinY = coordinatePin.bottom;
-  // Math.ceil
-  console.log(pinX, pinY);
   addrInput.value = Math.ceil(pinX) + ' ' + Math.ceil(pinY);
 
-
 }
-const guest = form.querySelector('#capacity');
 
 formRoom.addEventListener('input', function () {
   checkRoomCapacity()
-
 })
 guest.addEventListener('input', function () {
   checkGuestCapacity()
 })
+homeType.addEventListener('input', function () {
+  checkPriceCapacity()
+})
+priceType.addEventListener('input',function () {
+  checkPriceCapacity()
+} )
 
 const roomCapacity = {
   "1": [1],
@@ -52,6 +55,13 @@ const guestCapacity = {
   '3':[3],
   '0':[100]
 
+}
+
+const priceCapacity = {
+  'bungalow': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000
 }
 
 const checkRoomCapacity = function () {
@@ -74,4 +84,18 @@ if (roomOptions.includes(Number(formRoom.value))) {
   guest.reportValidity()
 }
 }
+
+const checkPriceCapacity = () => {
+  const priceOptions = priceCapacity[homeType.value]
+  priceType.min = priceOptions;
+  console.log(1);
+  if (priceOptions <  priceType.value) {
+    priceType.setCustomValidity('')
+  } else {
+    priceType.setCustomValidity('fail')
+    priceType.reportValidity();
+  }
+}
+
+
 
