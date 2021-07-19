01244822cdc4d93caeab6ef4ff67566c232770dd
Lenkeri <Lno4kamalt@yandex.ru>
2021-07-19 14:46:04 +0500

    работа над шестым заданием. сервер и данные с него

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import {showPopupElem,  getPinsForMap, arrPinsMap} from './blocks.js'
 import {getDisableForm, getUndisableForm} from './form.js'
+import {getData} from './server.js'
 
 export const map = document.querySelector('.map')
-
 export const mapPins = map.querySelector('.map__pins')
 export const templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
 export const templatePopup = document.getElementById('card').content.querySelector('.popup');
+const errorList = map.querySelector('.error-list')
+const errorText = errorList.querySelector('.content-err');
 // mapPins.addEventListener('click', function(evt) {
 //   console.log(evt.target.closest('.map__pin').dataset.index)
 // })
@@ -13,13 +15,12 @@ export const templatePopup = document.getElementById('card').content.querySelect
 export const activate = function () {
   if(isActive()){
     map.classList.remove('map--faded')
-    getPinsForMap(arrPinsMap)
+    getData(onSuccess, onError)
     getUndisableForm()
   }
 }
 
 const isActive = function () {
-
   if (map.classList.contains('map--faded')){
     return true
   }
@@ -29,3 +30,13 @@ return false
 getDisableForm()
 
 
+const onSuccess = function(data) {
+  console.log('onSuccess', data);
+  getPinsForMap(data)
+}
+
+const onError = function(error) {
+errorList.classList.remove('.visually-hidden')
+  errorText.textContent = ` Ошибка ${error}`;
+
+}
---
+++ js/server.js
@@ -0,0 +1,15 @@
+export const getData = function(onSuccess, onError){
+  const promise = fetch('https://21.javascript.pages.academy/keksobooking/data');
+  promise
+  .then((res) => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      throw new Error(`${res.status} — ${res.statusText}`)
+    }
+  })
+  .then((body) => {
+    onSuccess(body);
+  })
+  .catch(onError)
+}
